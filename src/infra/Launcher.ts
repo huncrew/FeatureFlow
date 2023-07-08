bdbe7c9ba14ee763bd42aeec2b8e5ce2bbbbe48d
huncrew <mrdalegrant@gmail.com>
2023-07-08 13:29:59 +0100

    cdk.json tsconfig

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,1 +1,6 @@
-// added launcher
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { DataStack } from './stacks/DataStack';
+
+const app = new App();
+new DataStack(app, 'DataStack');
