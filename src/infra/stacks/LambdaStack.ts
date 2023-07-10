6e6c5559fd402098b3215ee3bebefe887d10130c
huncrew <mrdalegrant@gmail.com>
2023-07-10 12:49:59 +0100

    Utils, http

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,16 +1,27 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
+interface FootBallTable extends StackProps {
+  footballDataTable: ITable;
+}
+
 export class LambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+  public readonly helloLambdaIntegration: LambdaIntegration;
+
+  constructor(scope: Construct, id: string, props: FootBallTable) {
     super(scope, id, props);
     
-    new LambdaFunction(this, 'HelloLambda', {
+    const helloLambda = new LambdaFunction(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'hello.main',
       code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
-    })
+    });
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
 }
