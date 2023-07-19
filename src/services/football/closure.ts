e228038c67a052790c53c3547e025876122a3a7c
huncrew <mrdalegrant@gmail.com>
2023-07-19 07:53:23 +0100

    marshalling added to get items

--- src/services/football/closure.ts
+++ src/services/football/closure.ts
@@ -1,6 +1,6 @@
 // lets nail this
 
-function dale(x?: number) {
+function dale(x?: number): void {
   if(x) {
     let hi = 'hi';
     var bye = 'bye'
@@ -8,4 +8,32 @@ function dale(x?: number) {
   // console.log(hi); // can't find name 'hi'
   console.log(bye);
 }
+
+function add(n: number) {
+  let number: number = n; // because its declared in the outer scope, i can access even using 'let'
+
+  return function() {
+    number += 1;
+    console.log(number)
+  }
+}
+
+/* right I get it, because a function is being returned,
+we are assigning the returning function to the variable 'closure'
+so, when we 'invoke closure' we are invoking that nested return function.
+and it actually has access to that internal variable while the app is running,
+so ONLY that returned function can access that scoped in variable.
+
+Because of how javascript works, when we assign a function to a variable, we assign whatever the return value is.
+*/ 
+
+var closure = add(1);
+closure();
+closure();
+
+
+
+
+
+
 
--- src/services/football/getFootball.ts
+++ src/services/football/getFootball.ts
@@ -1,22 +1,22 @@
 import { DynamoDBClient, GetItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { unmarshall } from "@aws-sdk/util-dynamodb"
 
 export async function getFootball(event: APIGatewayProxyEvent, dynamodbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
 
   if (event.queryStringParameters) {
-    console.log(event.queryStringParameters);
     const footballId = event.queryStringParameters['id'];
-    console.log(footballId);
     const singleItemResponse = await dynamodbClient.send(new GetItemCommand({
       TableName: process.env.TABLE_NAME,
       Key: {
         'id': {S: footballId}
       }
     }));
-    console.log(singleItemResponse.Item);
+
+    const unmarshResp = unmarshall(singleItemResponse.Item);
     return {
       statusCode: 201,
-      body: JSON.stringify({message: 'hi Dale', item: singleItemResponse.Item})
+      body: JSON.stringify({message: 'hi Dale', item: unmarshResp})
     }
   }
 
@@ -24,10 +24,10 @@ export async function getFootball(event: APIGatewayProxyEvent, dynamodbClient: D
     TableName: process.env.TABLE_NAME,
     }));
 
-  console.log(result);
+  const unmarshAllItems = result.Items.map(item => unmarshall(item));
 
   return { 
     statusCode: 201,
-    body: JSON.stringify({items: result.Items, dale: 'you did it'})
+    body: JSON.stringify({items: unmarshAllItems, dale: 'you did it'})
   }
 }
